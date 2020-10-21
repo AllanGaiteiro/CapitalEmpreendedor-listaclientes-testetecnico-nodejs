e6afcbfdec76a4ca2fd56aebbc379ed807843e7f
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2020-10-21 10:48:42 -0300

    update
    *falta corrigir a opsao delete

--- routes/admin.js
+++ routes/admin.js
@@ -36,19 +36,20 @@ router.get('/lista', function (req, res, next) {
 });
 
 //// visualizar cliente
-router.get('/ver/:key', function (req, res, next) {
+router.post('/ver', function (req, res, next) {
   //// Chamando dados cliente  usando a key pasada na lista
-  func.getOne('users', req.params.key).then(
+  func.getOne('users', req.body.key).then(
     (client) => {
       //// Chamando oportunidades do client  usando a key pasada na lista
-      func.getOne('opportunities', req.params.key).then(
+      func.getOne('opportunities', req.body.key).then(
         (oportClient) => {
           //// renderizando todos pagina com dados do cliente e suas oportunidades
-          res.render('admin/ver-client',
+          res.render('admin/verClient',
             {
               client: client,
-              opt: oportClient.opportunities
-            }
+              opt: oportClient.opportunities,
+              key: client.email
+            }, 
           );
         }
       )
@@ -78,15 +79,50 @@ router.post('/new', function (req, res, next) {
   }).then(
     () => {
       //console.log('Cliente Cadastrado com Sucesso')
-      func.set('opportunities', req.body.email, {"opportunities" : []} ).then(
-        console.log('adcionado as opportunities')
+      func.set('opportunities', req.body.email, { "opportunities": [] }).then(
+        res.render('admin/confirm', {acao: 'users'} )
       )
     }
   ).catch(
     (err) => {
       console.log('Erro ao Cadastrar o Cliente: ' + err)
     }
-  )})
+  )
+})
+
+//// Novo cliente
+router.post('/newOpt', function (req, res, next) {
+
+  /// pegar oportunidades ja cadastradas e acrescentando nova  
+  func.getOne('opportunities', req.body.KeyEmail).then( 
+    (e) => {
+      e.opportunities.push(
+        {
+          name: req.body.optName,
+          limit: Number(req.body.optLimit),
+          isActive: (req.body.optIsActive == 'on') ? true : false,
+          interest: Number(req.body.optInterest),
+          term: Number(req.body.optTerm),
+        }
+      )
+      func.set('opportunities', req.body.KeyEmail,  e )
+       
+      res.render('admin/confirm', {acao: 'opportunities'} )
+    }
+  )
+}
+)
+
+router.post('/del/', function (req, res, next) {
+  func.delete('users', req.body.key).then(
+    res.render('admin/lista')
+  )
+})
+router.post('/delOpt', function (req, res, next) {
+  console.log(req.body.key)
+})
 
 
-  module.exports = router;
+
+
+module.exports = router;
